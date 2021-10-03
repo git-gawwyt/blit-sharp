f91a54e38f9d870c0a1740a9e154acdd356e28c2
gareth.tucker <gareth.tucker@tcs-nz.co.nz>
2021-10-04 12:20:23 +1300

    create csproj

---
+++ blit-sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace blit_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
